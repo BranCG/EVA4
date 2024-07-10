0a5b4660924b5beef3cd7b85768eb579b842ae27
Brandon Castro García <b_castro@inacap.cl>
2024-07-10 13:14:39 -0400

    Agregue scrollreveal a la mi imagen

--- evaluacion4/src/ImagenChuck.tsx
+++ evaluacion4/src/ImagenChuck.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react"; 
+import ScrollReveal from "scrollreveal";  //para visualizar esto debe instalar scrollreveal -> npm install @types/scrollreveal
+
 
 export default function ImagenChuck() {
+    useEffect(() => {
+        const sr = ScrollReveal({
+            origin: 'top',
+            distance: '50px',
+            duration: 1000,
+            delay: 200,
+            reset: true
+        });
+        sr.reveal('.logo');
+    }, []);
+
     return (
-        <div >
+        <div>
             <img className="logo" src="https://avatars.githubusercontent.com/u/17794049?v=4" alt="Chuck Norris" />
         </div>
     );
 }
-
