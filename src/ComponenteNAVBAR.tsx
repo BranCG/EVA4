1027d6cc7ac5850badeb46a5d8cc094d687934d7
Brandon Castro <brandon.joao.c@gmail.com>
2024-07-13 16:54:25 -0400

    Mejoro redaccion de alertas

--- src/ComponenteNAVBAR.tsx
+++ src/ComponenteNAVBAR.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import LogoChuckNorris from './ComponenteLogo';
 
 export default function CompNav() {
-    const politicasPrivacidad = () => {
+    const politicasPrivacidad = () => { {/*Funcionalidad de eva3, politicas de privacidad*/}
         alert('POLÍTICA DE PRIVACIDAD\n\
 El presente Política de Privacidad establece los términos en que ChuckNorris usa y protege la información que es proporcionada por sus usuarios al momento de utilizar su sitio web. Esta compañía está comprometida con la seguridad de los datos de sus usuarios. Cuando le pedimos llenar los campos de información personal con la cual usted pueda ser identificado, lo hacemos asegurando que sólo se empleará de acuerdo con los términos de este documento. Sin embargo esta Política de Privacidad puede cambiar con el tiempo o ser actualizada por lo que le recomendamos y enfatizamos revisar continuamente esta página para asegurarse que está de acuerdo con dichos cambios.\n\
 Información que es recogida\n\
@@ -10,10 +10,10 @@ Nuestro sitio web podrá recoger información personal por ejemplo: Nombre,  inf
 Uso de la información recogida\n\
 Nuestro sitio web emplea la información con el fin de proporcionar el mejor servicio posible, particularmente para mantener un registro de usuarios, de pedidos en caso que aplique, y mejorar nuestros productos y servicios.  Es posible que sean enviados correos electrónicos periódicamente a través de nuestro sitio con ofertas especiales, nuevos productos y otra información publicitaria que consideremos relevante para usted o que pueda brindarle algún beneficio, estos correos electrónicos serán enviados a la dirección que usted proporcione y podrán ser cancelados en cualquier momento.');
     };
-    const queHacer = () => {    
+    const queHacer = () => {     {/*Explicacion de que hacer*/}
         alert('DISFRUTA FRASES DE CHUCK NORRIS\n\
-Puedes optar por una frase instantanea en esta web o por otra parte Suscribirte para recibir frases epicas y unicas del increible Chuck Norris!\n\
-Quedate el tiempo que quieras, pasa a visitarnos cuando quieras, la experiencia es GRATIS! :)\n\
+\n\ Puedes optar por una frase instantanea en esta web o por otra parte Suscribirte llenando el formulario para recibir frases epicas y unicas del increible Chuck Norris en tu correo electronico!\n\
+\n\ Quedate el tiempo que quieras, pasa a visitarnos cuando quieras, la experiencia es GRATIS! :)\n\
 ');
     };
 
