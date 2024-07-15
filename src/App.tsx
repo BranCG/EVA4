34a33a4635f9c299fc0c9a2d7650b775792e7bc0
Brandon Castro <Brandon.joao.c@gmail.com>
2024-07-15 15:46:10 -0400

    Modifico comentarios, TRABAJO LISTO!

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css'; 
-import 'bootstrap/dist/css/bootstrap.min.css';  //importo Booststrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 import CompNav from './Component/ComponenteNAVBAR'; 
 import ImagenChuck from './Component/ComponenteImagenChuck';
 import CompFooter1 from './Component/CompFooter';
 import CompBtnFrase from './Component/ComponenteFrase';
 import CompLogicaCrud from './Component/ComponenteLogicaCrud';
-//En total realice 10 componentes, estan todos en carpeta "Component", el componente anidado nivel 4 se llama <CompLogicCrud />
-//Profesor, favor instalar libreria scrollReveal para funcionalidad "npm install scrollreveal".
+//En total realice 10 componentes, estan todos en carpeta "Component", el componente anidado nivel 4 empieza en <CompLogicCrud />
+//Profesor, favor instalar libreria scrollReveal y Booststrap para funcionalidad "npm install scrollreveal/Bootstrap".
 //App.css uso estilos para las CLASES que cree.
-//index.css uso los estilos para los ELEMENTOS/ETIQUETAS HTML
+//index.css uso los estilos para los ELEMENTOS/ETIQUETAS HTML.
 
 const App: React.FC = () => {  
     return (
